033569e69d476895fee959c3be58473581fb5257
Viktor Pec <viktor.pec@fzu.cz>
2025-01-16 17:22:48 +0100

    Reworked trimming script to allow to create larger files.

--- trim_file.py
+++ trim_file.py
@@ -1,11 +1,25 @@
 import uproot as ur
 import sys
 
+BATCH_SIZE=20000
+MAX_SIZE=int(sys.argv[3])
 
-data=0
-with ur.open(sys.argv[1]+':raw_waveforms') as tree :
-    data = tree.arrays(['adcs','channel'],entry_stop=int(sys.argv[3]))
+outf = ur.recreate(sys.argv[2])
 
+stored = 0
+iteration = 0
+for data in  ur.iterate(sys.argv[1]+':raw_waveforms',['adcs','channel'],step_size=BATCH_SIZE) :
+    if stored >= MAX_SIZE :
+        break
 
-with ur.recreate(sys.argv[2]) as outf :
-    outf['raw_waveforms'] = {'adcs':data.adcs, 'channel':data.channel}
+    print(f'Storing batch {iteration} of size {len(data.channel)}')
+
+    if stored == 0 :
+        outf['raw_waveforms'] = {'adcs':data.adcs, 'channel':data.channel}
+    else :
+        outf['raw_waveforms'].extend({'adcs':data.adcs, 'channel':data.channel})
+
+    stored += len(data.channel)
+    iteration += 1
+
+outf.close()
